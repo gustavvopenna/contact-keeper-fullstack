401ead1706b6db2ce5da9b53a751d441a6e9aea8
gustavvopenna <gustavo.pema@gmail.com>
2019-06-21 01:11:59 -0500

    Add function to create new users

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+
+const User = require('../models/User')
 
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
-router.post('/', (req, res) => {
-  res.send('Register a user')
-})
+router.post(
+  '/',
+  [
+    check('name', 'Please add your name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with at least 6 characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { name, email, password } = req.body
+
+    User.create({
+      name,
+      email,
+      password
+    })
+
+    res.status(200).json('User created')
+  }
+)
 
 module.exports = router
